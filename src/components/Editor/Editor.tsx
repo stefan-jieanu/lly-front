231bab0238425f28318b3f7c8881adff2fe505fa
stefan-jieanu <stefanalexjieanu@gmail.com>
2023-11-29 21:54:11 +0200

    added tailwind

---
+++ src/components/Editor/Editor.tsx
@@ -0,0 +1,14 @@
+export default function Editor() {
+  return (
+    <div className="h-full">
+      <div className='flex gap-10 h-full items-center justify-center'>
+        <div className='p-20 rounded-2xl shadow-[5px_5px_20px_0px_rgba(0,0,0,0.8)] bg-slate-900'>
+          <h1 className='text-3xl text-white'>Hello world</h1>
+        </div>
+        <div className='p-20 rounded-2xl shadow-[5px_5px_20px_0px_rgba(0,0,0,0.8)] bg-slate-900'>
+          <h1 className='text-3xl text-white'>Hello world</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
